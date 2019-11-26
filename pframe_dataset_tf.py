c46c56249e2d2567fb14c4ac8bce09f2b5aaa6b6
Fabian <fmentzer@me.com>
2019-11-26 17:42:28 +0100

    cleanup

--- pframe_dataset_tf.py
+++ pframe_dataset_tf.py
@@ -1,6 +1,19 @@
 import pframe_dataset_shared
 import tensorflow as tf
 
+
+def frame_pairs_dataset(data_root, merge_channels=False):
+    # all frames, i.e., frames 0, 1, 2, 3, ...
+    d_a = yuv_dataset(data_root)
+    # same, but skip first frame, i.e., frames 1, 2, 3, ...
+    d_b = yuv_dataset(data_root).skip(1)
+    if merge_channels:
+        d_a = d_a.map(yuv_422_to_444)
+        d_b = d_b.map(yuv_422_to_444)
+    # zip together, so we get pairs (0, 1), (1, 2), (2, 3), ...
+    return tf.data.Dataset.zip((d_a, d_b))
+
+
 def yuv_422_to_444(y, u, v):
     """ Convert Y, U, V, given in 422, to RGB 444. """
     u, v = map(_upsample_nearest_neighbor, (u, v))  # upsample U, V
@@ -25,14 +38,3 @@ def yuv_dataset(data_root):
                for glob in pframe_dataset_shared.get_yuv_globs(data_root))
     return tf.data.Dataset.zip((y, u, v))
 
-
-def frame_pairs_dataset(data_root, merge_channels=False):
-    # all frames, i.e., frames 0, 1, 2, 3, ...
-    d_a = yuv_dataset(data_root)
-    # same, but skip first frame, i.e., frames 1, 2, 3, ...
-    d_b = yuv_dataset(data_root).skip(1)
-    if merge_channels:
-        d_a = d_a.map(yuv_422_to_444)
-        d_b = d_b.map(yuv_422_to_444)
-    # zip together, so we get pairs (0, 1), (1, 2), (2, 3), ...
-    return tf.data.Dataset.zip((d_a, d_b))
