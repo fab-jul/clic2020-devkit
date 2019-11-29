9712baa62e2f993833f6bfee8f8dc7704d5e8624
Fabian <fmentzer@me.com>
2019-11-29 15:44:33 +0100

    more detailed errors in download validation

--- pframe_dataset_shared.py
+++ pframe_dataset_shared.py
@@ -1,44 +1,60 @@
 import glob
+import sys
 import argparse
 import os
+import re
+
+
+_SUFFIXES = ('_y.png', '_u.png', '_v.png')
 
 
 def get_yuv_globs(data_root):
     """
     Expected structure:
     `root_dir`/
-      video1/
-        video1_frame1_y.png
-        video1_frame1_u.png
-        video1_frame1_v.png
-        video1_frame2_y.png
-        ...
-      video2/
-        ...
+      video1_frame1_y.png
+      video1_frame1_u.png
+      video1_frame1_v.png
+      video1_frame2_y.png
+      ...
+      video2_frame1_y.png
+      video2_frame1_u.png
+      video2_frame1_v.png
+      video2_frame2_y.png
+      ...
 
     :returns globs (as strings) for Y, U, V frames
     """
-    subfolder_glob = os.path.join(data_root, '*')
-    y_glob, u_glob, v_glob = (os.path.join(subfolder_glob, '*' + suffix)
-                              for suffix in ('_y.png', '_u.png', '_v.png'))
+    y_glob, u_glob, v_glob = (os.path.join(data_root, '*' + suffix)
+                              for suffix in _SUFFIXES)
     return y_glob, u_glob, v_glob
 
 
 def validate_data(data_root):
     """ Check if for every frame we have Y, U, V files. TODO: call from download.sh """
     globs = get_yuv_globs(data_root)
-    files = tuple(glob.glob(g) for g in globs)
-    assert len(files[0]) > 0, 'No files found in {}'.format(data_root)
-    assert len(set(map(len, files))) == 1, 'Expected y, u, v file for every frame! Got {}'.format(list(map(len, files)))
-    print('Found {} frames, and Y, U, V for each'.format(len(files[0])))
+    all_ps = tuple(sorted(glob.glob(g)) for g in globs)
+    assert len(all_ps[0]) > 0, 'No files found in {}'.format(data_root)
+    # get a set of prefixes for each Y, U, V by replacing the suffix of each path
+    ys_pre, us_pre, vs_pre = (set(re.sub(suffix + '$', '', p) for p in ps)
+                              for suffix, ps in zip(_SUFFIXES, all_ps))
+    if len(ys_pre) == len(us_pre) == len(vs_pre):
+        print('Found {} frames, and Y, U, V for each'.format(len(ys_pre)))
+        return 0
 
+    all_frames = ys_pre | us_pre | vs_pre
+    ys_missing, us_missing, vs_missing = (all_frames - pre for pre in (ys_pre, us_pre, vs_pre))
+
+    print('ERROR:\nMissing Y for: {}\nMissing U for: {}\nMissing V for: {}'.format(
+            ys_missing or '-', us_missing or '-', vs_missing or '-'))
+    return 1
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('--validate', metavar='DATA_ROOT_FOLDER')
     flags = p.parse_args()
     if flags.validate:
-        validate_data(flags.validate)
+        sys.exit(validate_data(flags.validate))
 
 
 if __name__ == '__main__':
