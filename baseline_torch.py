38dd60f2d29899ba457cb009879ad1e755d37d6d
Fabian <fmentzer@me.com>
2019-12-03 17:52:23 +0100

    work in progress

--- baseline_torch.py
+++ baseline_torch.py
@@ -15,3 +15,62 @@ Algorithm:
       1. get Residual_Normalized from JPG in Bistream
       2. F2' = F1 + ( Residual_normalized - 127 ) * 2
 """
+import torch
+import argparse
+from io import BytesIO
+from PIL import Image
+from pframe_dataset_torch import FramePairsDataset
+import numpy as np
+
+def encoder(frame1, frame2):
+    # Convert to long so that the substraction does not overflow
+    residual_normalized = (frame2.astype(np.long) - frame1) // 2 + 127
+    print(np.amax(residual_normalized), np.amin(residual_normalized))
+    # Convert back to uint8
+    residual_normalized = residual_normalized.astype(np.uint8)
+    f = BytesIO()
+    Image.fromarray(residual_normalized).save(f, format='jpeg')
+    return f.getvalue()
+
+
+def decoder(frame1, frame2_compressed: bytes):
+    residual_normalized = np.array(Image.open(BytesIO(frame2_compressed)))
+    return frame1 + (residual_normalized - 127) * 2
+
+
+def torch_image_to_numpy(t):
+    """ Convert CHW normalized float32 (image) torch tensor to HWC numpy array """
+    return t.mul(255).round().to(torch.uint8).permute(1, 2, 0).detach().cpu().numpy()
+
+
+def compress_folder(data_dir):
+    ds = FramePairsDataset(data_dir) #, merge_channels=True)
+    ds.yuv_frames_dataset.image_to_tensor = lambda pic: np.array(pic)
+    idxs = np.arange(len(ds))
+    np.random.shuffle(idxs)
+    for i in idxs:
+        (y1, u1, v1), (y2, u2, v2) = ds[i]
+
+        num_bytes = 0
+        num_pixels = np.prod(y1.shape)
+
+        for c1, c2 in zip((y1, u1, v1), (y2, u2, v2)):
+            b = encoder(c1, c2)
+            num_bytes += len(b)
+            c2_decoded = decoder(c1, b)
+            num_pixels_c = np.prod(c1.shape)
+            ms_ssim_weight = 1 # todo check for MS-SSIM code
+
+    # TODO output metrics
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('data_dir')
+    flags = p.parse_args()
+
+    compress_folder(flags.data_dir)
+
+
+if __name__ == '__main__':
+    main()
