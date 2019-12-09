83d6b428c98409ed489b3c5e92438f694e8129a4
Fabian <fmentzer@me.com>
2019-12-09 10:31:52 +0100

    finalized baseline

--- baseline_torch.py
+++ baseline_torch.py
@@ -21,11 +21,16 @@ from io import BytesIO
 from PIL import Image
 from pframe_dataset_torch import FramePairsDataset
 import numpy as np
+import ms_ssim_np
+
+
+_MSSSIM_WEIGHTS = (1/1.5, .25/1.5, .25/1.5)
+
 
 def encoder(frame1, frame2):
     # Convert to long so that the substraction does not overflow
     residual_normalized = (frame2.astype(np.long) - frame1) // 2 + 127
-    print(np.amax(residual_normalized), np.amin(residual_normalized))
+    # print(np.amax(residual_normalized), np.amin(residual_normalized))
     # Convert back to uint8
     residual_normalized = residual_normalized.astype(np.uint8)
     f = BytesIO()
@@ -46,22 +51,32 @@ def torch_image_to_numpy(t):
 def compress_folder(data_dir):
     ds = FramePairsDataset(data_dir) #, merge_channels=True)
     ds.yuv_frames_dataset.image_to_tensor = lambda pic: np.array(pic)
-    idxs = np.arange(len(ds))
+    N = len(ds)
+    idxs = np.arange(N)
     np.random.shuffle(idxs)
-    for i in idxs:
+    metrics = []
+    for count, i in enumerate(idxs):
         (y1, u1, v1), (y2, u2, v2) = ds[i]
 
         num_bytes = 0
         num_pixels = np.prod(y1.shape)
+        ms_ssim = 0
 
-        for c1, c2 in zip((y1, u1, v1), (y2, u2, v2)):
+        for ms_ssim_weight, c1, c2 in zip(_MSSSIM_WEIGHTS, (y1, u1, v1), (y2, u2, v2)):
             b = encoder(c1, c2)
             num_bytes += len(b)
             c2_decoded = decoder(c1, b)
-            num_pixels_c = np.prod(c1.shape)
-            ms_ssim_weight = 1 # todo check for MS-SSIM code
+            ms_ssim_c = ms_ssim_np.MultiScaleSSIM(_batch(c1), _batch(c2_decoded))
+            ms_ssim += ms_ssim_weight * ms_ssim_c
+
+        bpp = num_bytes * 8 / num_pixels
+        metrics.append((bpp, ms_ssim))
+        print('{: 10d}/{}: {:.4f} bpp // {:.4f} weighted MS-SSIM'.format(count, N, bpp, ms_ssim))
 
     # TODO output metrics
+
+def _batch(c):
+    return np.expand_dims(np.expand_dims(c, -1), 0)
 
 
 def main():
